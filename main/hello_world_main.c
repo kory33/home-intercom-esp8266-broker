a5770460e0c68b2057b95fb0e414237e2cb84452
Kory <korygm33@gmail.com>
2021-06-11 06:00:26 +0900

    [update] accept input from the serial

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,33 +1,74 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
+#include <string.h>
+#include <esp_vfs_dev.h>
+
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_spi_flash.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "nvs.h"
 
 #include "driver/uart.h"
 
+static void processConfirmStartupCommand() {
+    printf("STARTED\t");
+    fflush(stdout);
+}
+
+static void processNotifySoundDetectionCommand() {
+    printf("DETECTED\t");
+    fflush(stdout);
+}
+
+static void processConfirmRemoteAliveCommand() {
+    printf("REMOTE ALIVE\t");
+    fflush(stdout);
+}
+
+_Noreturn static void process_command_loop(__attribute__((unused)) void *pvParameters) {
+    for (;;) {
+        unsigned char command_input[2];
+        const int read_length = uart_read_bytes(CONFIG_CONSOLE_UART_NUM, command_input, 1, 1);
+        if (read_length > 0) {
+            uart_flush_input(CONFIG_CONSOLE_UART_NUM);
+            const unsigned char read_character = command_input[0];
+
+            switch (read_character) {
+                case 'C':
+                    processConfirmStartupCommand();
+                    continue;
+                case 'N':
+                    processNotifySoundDetectionCommand();
+                    continue;
+                case 'R':
+                    processConfirmRemoteAliveCommand();
+                    continue;
+                default:
+                    continue;
+            }
+        }
+    }
+}
+
 __attribute__((unused)) void app_main()
 {
-    uart_set_baudrate(UART_NUM_0, 115200);
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    printf("Hello world!\n");
+    /**
+     * Initially baudrate is 115200 * 26 / 40 = 74880Hz
+     * due to the crystal operating at 26MHz instead of 40MHz.
+     */
+    uart_set_baudrate(CONFIG_CONSOLE_UART_NUM, 115200);
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
-           chip_info.cores);
+    /**
+     * initialize stdin/out
+     * https://github.com/espressif/esp-idf/issues/4564#issuecomment-569889317
+     */
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    ESP_ERROR_CHECK(uart_driver_install((uart_port_t)CONFIG_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0));
+    esp_vfs_dev_uart_use_driver(CONFIG_CONSOLE_UART_NUM);
+    esp_vfs_dev_uart_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+    esp_vfs_dev_uart_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
 
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    xTaskCreate(&process_command_loop, "process_cmd_loop", 1024, NULL, 5, NULL);
 }
